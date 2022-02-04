e29558982a89fe7b63fcfa42f239ea74d036a049
Eduardo Soares <filhodudu00@gmail.com>
2022-02-04 09:59:40 -0300

    Add files via upload

---
+++ aula013/ambiente.js
@@ -0,0 +1,13 @@
+var cont = 1
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 6)
+
+/* 
+Testa primeiro e executa o bloco depois
+while (cont <= 6){
+    console.log(`Passo ${cont}`)
+    cont ++
+}
+*/
---
+++ aula014/ambiente.js
@@ -0,0 +1,8 @@
+/*for(inicio ; teste lógico ; incremento){
+    comando
+} */
+
+console.log('Vai começar...')
+for (var c=1; c <=6 ; c++){
+    console.log(`Passo ${c}`)
+}
