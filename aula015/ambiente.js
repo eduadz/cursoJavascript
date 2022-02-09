c6385803feff54676ee15560405e0e1ca411cea0
Eduardo Soares <filhodudu00@gmail.com>
2022-02-09 15:59:51 -0300

    09/02

---
+++ aula015/ambiente.js
@@ -0,0 +1,11 @@
+let num  = [2,1,8,6,70,200,12,33,64]
+/* ordem crescente: num.sort(function(a,b){return a-b});
+ '' descrescente : ''{return b-a});
+while (c <= num.length){
+    console.log(num[c])
+    c++
+}*/
+
+for(let c in num)  {
+   console.log(num[c]) 
+}
